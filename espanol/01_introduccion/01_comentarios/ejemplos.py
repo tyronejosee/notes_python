153d882baf2328402cf2453a2c179659377b3625
tyronejose <alt.tyronejose@gmail.com>
2023-06-26 14:47:54 -0400

    Update 26_06_2023 4

--- espanol/01_introduccion/01_comentarios/ejemplos.py
+++ espanol/01_introduccion/01_comentarios/ejemplos.py
@@ -1,3 +1,33 @@
 """
 Ejemplos de Comentarios
 """
+
+### Creando un comentario
+# Los comentarios comienzan con un # y Python los ignorará
+
+# Este es un comentario
+print("¡Hola, Mundo!")
+
+# Los comentarios se pueden colocar al final de una línea
+print("¡Hola, Mundo!") # Este es un comentario
+
+# También puedes usar los comentarios para evitar que Python ejecute código
+#print("¡Hola, Mundo!")
+print("¡Saludos, amigo!")
+
+### Comentarios de varias líneas
+# Python no tiene realmente una sintaxis para comentarios de varias líneas
+# Para agregar un comentario de varias líneas, puedes insertar un # en cada línea
+
+#Este es un comentario
+#escrito en
+#más de una línea
+print("¡Hola, Mundo!")
+
+# Usar string para comentar
+"""
+Este es un comentario
+escrito en
+más de una línea
+"""
+print("¡Hola, Mundo!")
